e1e9d97c96b643835a50c834a91f9dfbc1d8b615
jake2255 <157094404+jake2255@users.noreply.github.com>
2026-09-06 20:18:41 -0500

    initiated project

---
+++ phone to pc transfer app/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace phone_to_pc_transfer_app
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ phone to pc transfer app/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace phone_to_pc_transfer_app
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}

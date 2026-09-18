fd3f0caf656ec679759e2bc5a6c9384e7bd4a4a9
jake2255 <157094404+jake2255@users.noreply.github.com>
2026-09-17 20:16:55 -0500

    kestrel server will not work on android, shifting to raw TCP for tranfers. reworking transfer code

---
+++ phone to pc transfer app/Core/TransferProtocol.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace phone_to_pc_transfer_app.Core
+{
+    public class TransferHeader
+    {
+        public string Type { get; set; } = string.Empty;
+        public string FileName { get; set; } = string.Empty;
+        public long PayloadLength { get; set; }
+
+    }
+    public static class TransferProtocol
+    {
+        public const string TypeFile = "file";
+        public const string TypeText = "text";
+        private const int MaxHeaderBytes = 64 * 1024;
+
+        public static async Task WriteHeaderAsync()
+        {
+
+        }
+
+        public static async Task<TransferHeader?> ReadHeaderAsync()
+        {
+
+        }
+
+        public static async Task CopyExactlyAsync()
+        {
+
+        }
+
+        private static async Task<bool> ReadExactlyAsync()
+        {
+
+        }
+    }
+}
--- phone to pc transfer app/Core/TransferServer.cs
+++ phone to pc transfer app/Core/TransferServer.cs
@@ -1,14 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Net.Sockets;
 using System.Text;
 
 namespace phone_to_pc_transfer_app.Core
 {
-    public class TransferServer : IAsyncDisposable
+    public class TransferServer : IDisposable
     {
         private readonly int _port;
         private readonly string _saveFolder;
-        private WebApplication? _app;
+        private TcpListener? _listener;
+        private CancellationTokenSource? _cts;
+        private Task? _acceptLoop;
 
         public event EventHandler<FileReceivedEventArgs>? FileReceived;
 
